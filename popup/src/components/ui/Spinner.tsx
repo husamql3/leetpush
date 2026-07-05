48843573d979c388785279127c2d7a08e83ce3be
Hüsam <husam.yildirim@firstimpactmed.com>
2026-07-05 13:11:46 +0300

    chore: release v1.8.2

--- popup/src/components/ui/Spinner.tsx
+++ popup/src/components/ui/Spinner.tsx
@@ -1,13 +1,7 @@
-import { Spin } from "antd";
-import { LoadingOutlined } from "@ant-design/icons";
-
 export default function Spinner() {
   return (
     <div className="flex justify-center py-32">
-      <Spin
-        indicator={<LoadingOutlined className="text-lp-yellow" spin />}
-        size="large"
-      />
+      <div className="h-8 w-8 animate-spin rounded-full border-4 border-lp-yellow border-t-transparent" />
     </div>
   );
 }
